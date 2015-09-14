5150664c4ee3a3151e94e87b63f3f80a8ab541f3
sergeiz <zaychenko.sergei@gmail.com>
2015-09-14 10:53:17 +0300

    Лаба №1. Вариант Б-10. Единица длины

---
+++ lab1/b10_lengthunit/lengthunit/lengthunit.cpp
@@ -0,0 +1,16 @@
+// (C) 2013-2015, Sergei Zaychenko, KNURE, Kharkiv, Ukraine
+
+#include "lengthunit.hpp"
+
+#include <stdexcept>
+
+/*****************************************************************************/
+
+const long double LengthUnit::METERS_IN_INCH = 0.0254;
+const long double LengthUnit::METERS_IN_FOOT = 0.3048;
+
+/*****************************************************************************/
+
+// TODO ...
+
+/*****************************************************************************/
